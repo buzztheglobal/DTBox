43d8be7b607866aacaf4e35ae117a8dac373f179
DailyToolbox <buzz.theglobal@gmail.com>
2025-08-14 23:03:11 +0530

    only Active cards/Tools are visible now

--- backend/controllers/menuItemController.js
+++ backend/controllers/menuItemController.js
@@ -3,7 +3,8 @@ const pool = require('../config/db');
 
 exports.getAllMenuItems = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM menu_menuitem WHERE is_deleted = false');
+    const strQuery = 'SELECT * FROM menu_menuitem WHERE is_deleted = false  and is_active=TRUE and is_visible=TRUE order by url, tool_domain';
+    const result = await pool.query(strQuery);
     res.status(200).json(result.rows);
   } catch (error) {
     console.error('❌ Error fetching menu items:', error);
